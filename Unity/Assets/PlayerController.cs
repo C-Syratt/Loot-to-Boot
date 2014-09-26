69fac0cebc55cba79292f87eafbf5a87f34bd751
Silcoish <silcoish@gmail.com>
2014-09-27 09:16:46 +1000

    Started the Projected
    Created the project, made an ignore file and completed horizontal
    movement within the game

---
+++ Unity/Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float maxVelocity = 10f;
+
+	void FixedUpdate(){
+		if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0){
+			transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal") * maxVelocity, transform.position.y);
+		}
+	}
+}
