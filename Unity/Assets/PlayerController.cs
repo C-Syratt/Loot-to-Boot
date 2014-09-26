97a2260393cefac82e7cd91c6426d10c73bed123
Silcoish <silcoish@gmail.com>
2014-09-27 09:48:50 +1000

    Made Jump
    Changed the player movement script a little to allow for the player to
    jump - adjust variables in inspector to change height

--- Unity/Assets/PlayerController.cs
+++ Unity/Assets/PlayerController.cs
@@ -2,12 +2,36 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-	[SerializeField] float maxVelocity = 10f;
-
-	void FixedUpdate(){
-		if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0){
-			transform.position = new Vector2(transform.position.x + Input.GetAxis("Horizontal") * maxVelocity, transform.position.y);
-		}
+	
+	[SerializeField] float maxSpeed = 10f;
+	bool jump = false;
+	bool grounded = false;
+	[SerializeField] float groundRadius = 0.2f;
+	[SerializeField] float jumpForce = 0f;
+	
+	Transform groundCheck;
+	[SerializeField] LayerMask groundObjects;
+	Animator anim;
+	
+	void Start(){
+		groundCheck = transform.Find("GroundCheck");
 	}
+	
+	void Update(){
+		if(grounded && Input.GetButtonDown("Jump"))
+			jump = true;
+		
+		rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, rigidbody2D.velocity.y);
+	}
+	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundObjects);
+		
+		if(jump){
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+		
+	}
+	
 }
