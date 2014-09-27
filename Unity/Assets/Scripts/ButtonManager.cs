75fdb75962b112cb65ddeefb9d0663d500c15bae
C-Syratt <c.syratt@aanet.com.au>
2014-09-27 16:51:44 +1000

    Grey box Menu Implemented
    | Menu Scene
    + Buttons Function
    + Credits
    
    | Placeholder Loot
    + Sword, bow, pike/spears sprites
    
    | Misc
    + Added folders for Scripts & Placeholder Art

---
+++ Unity/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	[SerializeField]GameObject cam;
+	string buttonName;
+	
+	void OnMouseDown()
+	{
+		// set the var buttonName to the name of the button click and run ButtonPressed func
+		buttonName = gameObject.name;
+		ButtonPressed (buttonName);
+	}
+
+
+	// rather than make an individual script for each button
+	// I am running the names through a switch statement to easily see what each button does
+	void ButtonPressed(string buttonName)
+	{
+		switch (buttonName) 
+		{
+		// Main Menu Screen buttons
+		case "Play":
+			Application.LoadLevel(1);
+			break;
+
+		case "Credits":
+			cam.SendMessage("moveCam", buttonName);
+			break;
+
+		case "Exit":
+			print ("Exit");
+			Application.Quit();
+			break;
+
+		// Credits Screen buttons
+		case "Back":
+			cam.SendMessage("moveCam", buttonName);
+			break;
+
+		}
+
+	}
+
+
+}
---
+++ Unity/Assets/Scripts/menuCam.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuCam : MonoBehaviour {
+
+	[SerializeField]float camSpeed;
+
+
+	Vector3 newPos;
+	Vector3 mainPos = new Vector3(0,0,-10);
+	Vector3 creditPos = new Vector3(15,0,-10);
+
+
+	void Start () 
+	{
+		// so the camera does not move to (0,0,0) before a button is pressed
+		newPos = mainPos;
+	}
+	
+	void Update () 
+	{
+		transform.position = Vector3.MoveTowards(transform.position, newPos, camSpeed * Time.deltaTime);
+	}
+
+	void moveCam(string buttonPressed)
+	{
+		switch (buttonPressed) 
+		{
+		case "Credits":
+			newPos = creditPos;
+			break;
+
+		case "Back":
+			newPos = mainPos;
+			break;
+
+		}
+	}
+
+
+
+}
