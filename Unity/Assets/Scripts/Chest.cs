2b58e86ffd07e18d25a6eaefaeafa6f786c741e1
Silcoish <silcoish@gmail.com>
2014-09-27 23:02:12 +1000

    Made loot fly
    | Game Scene
    + Added new folder for gameobjects
    
    | Placeholder Art
    + Created a few placeholder arts
    
    | Functionality
    + Created a script for the chest
    + Created a script for loot

---
+++ Unity/Assets/Scripts/Chest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+	[SerializeField] GameObject[] loot;
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Player"){
+			Instantiate(loot[Random.Range(0, loot.Length)], transform.position, Quaternion.identity);
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Loot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loot : MonoBehaviour {
+
+	bool grounded = false;
+	[SerializeField] [Range(0, 10)] float maxHeight = 4;
+	[SerializeField] [Range(0, 10)] float minHeight = 2;
+	[SerializeField] [Range(0, 10)] float distanceX = 2;
+
+	Vector3 vel = new Vector3(2, 2, 0);
+	Vector3 grav = new Vector3(0, -2, 0);
+	[SerializeField] float groundRadius = 0.2f;
+	[SerializeField] LayerMask groundObjects;
+	Transform groundCheck;
+
+	void Start(){
+		vel = new Vector3(Random.Range(-distanceX, distanceX), Random.Range(minHeight, maxHeight), 0f);
+		grav = new Vector3(0f, Random.Range(-minHeight, -maxHeight), 0f);
+	}
+	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(transform.position, groundRadius, groundObjects);
+		if(!grounded){
+			transform.position = transform.position + vel * Time.deltaTime;
+			vel = vel + grav * Time.deltaTime;
+		}
+	}
+}
